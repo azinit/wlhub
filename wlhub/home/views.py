20f94f18a80ffcf658f45bb361449686dc3ecea7
Ilya Azin <martis.azin@gmail.com>
2020-05-17 01:27:11 +0300

    add component usage in templates

--- wlhub/home/views.py
+++ wlhub/home/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "book": "The Witcher 3"
+        "book": {
+            "author": "Andjei Sapkovskiy",
+            "name": "The Witcher 3",
+            "category": "Fantasy"
+        }
     }
     return render(request, 'home/index.html', context)
