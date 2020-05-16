1643ddd74507e364599c5dd1ab94f7955262bd98
Ilya Azin <martis.azin@gmail.com>
2020-05-17 02:32:45 +0300

    split home template

--- wlhub/home/views.py
+++ wlhub/home/views.py
@@ -2,11 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {
-        "book": {
-            "author": "Andjei Sapkovskiy",
-            "name": "The Witcher 3",
-            "category": "Fantasy"
-        }
-    }
+    context = {}
     return render(request, 'home/index.html', context)
