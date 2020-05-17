1d5ed102fc2516f61c1576040486694083bd3b90
Ilya Azin <martis.azin@gmail.com>
2020-05-17 03:37:45 +0300

    add task-details page

--- wlhub/tasks/views.py
+++ wlhub/tasks/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from core.utils import get_or_none
+from tasks.models import Task
+
 
 def index(request):
     context = {}
     return render(request, 'tasks/index.html', context)
+
+
+def task_details(request, pk: int):
+    task = get_or_none(Task, pk=pk)
+    context = {
+        "task": task
+    }
+    return render(request, 'task/index.html', context)
