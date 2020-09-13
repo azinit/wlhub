9fb2393145b6cc1e132581526217a7a980b6ecb5
Ilya Azin <martis.azin@gmail.com>
2020-09-13 21:18:12 +0300

    refactor: remove redundant

--- wlhub/api/urls.py
+++ wlhub/api/urls.py
@@ -9,8 +9,6 @@ from .views import *
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('tags-old', TagsAPIView.as_view(), name='api-tags-old'),
-    # path('tags', TagsViewSet.as_view(), name='api-tags'),
 ]
 
 app_name = 'api'
