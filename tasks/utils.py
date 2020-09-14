00791a4a9b0d634a2caf83d6bf4fb6144879d96e
Ilya Azin <martis.azin@gmail.com>
2020-09-14 23:56:05 +0300

    fix: add superaccess for admin

--- tasks/utils.py
+++ tasks/utils.py
@@ -5,5 +5,5 @@ def validate_task_access(user, task):
     """
     Валидация: Имеет ли пользователь доступ к задаче
     """
-    if task.user != user:
+    if task.user != user and not user.is_superuser:
         raise PermissionDenied()
