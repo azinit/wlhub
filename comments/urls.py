a2a7bf192d2a863e8435891bc9a579b04a37cda7
Ilya Azin <martis.azin@gmail.com>
2020-09-14 23:29:55 +0300

    refactor: add re_path

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
     # path('', task_list, name='comments'),
-    path('<int:pk>/new', comments_create, name='comment--create'),
+    re_path(r'^(?P<pk>\d+)/new$', comments_create, name='comment--create'),
+    # path('<int:pk>/new', comments_create, name='comment--create'),
     # path('<int:pk>/edit', task_edit, name='tasks-details--edit'),
     # path('<int:pk>/delete', task_delete, name='tasks-details--delete'),
     # path('<int:pk>', task_details, name='tasks-details'),
