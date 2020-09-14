b8b239a12fdd5f7f91cbd039cf88a3501d49f6dc
Ilya Azin <martis.azin@gmail.com>
2020-09-14 23:12:58 +0300

    fix: media loading

--- wlhub/urls.py
+++ wlhub/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', includes('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -38,6 +39,10 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+# errors handling
+handler404 = "core.views.error_404"
+handler500 = "core.views.error_500"
+
 # Routing
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -49,18 +54,18 @@ urlpatterns = [
     path('', include('home.urls')),
     path('debug/404', debug_404, name="debug-404"),
     path('debug/500', debug_500, name="debug-500"),
+    # For media handling with DEBUG=True and DEBUG=False
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 # static
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# media
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # swagger
 docs_urls = [
     path('swagger/', schema_view.with_ui('swagger'), name='schema-swagger-ui'),
 ]
 urlpatterns += [path('docs/', include(docs_urls))]
 
-# errors handling
-handler404 = "core.views.error_404"
-handler500 = "core.views.error_500"
+# if settings.DEBUG:
+#     # media
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
